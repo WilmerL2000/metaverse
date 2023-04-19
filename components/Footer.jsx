5d1b9b57fa6ef49d5cfb86fbaa62e3555809815e
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-04-19 17:08:22 -0600

    Adding some sections and components, and the images that we'll use

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+'use client';
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+
+const Footer = () => <footer>footer</footer>;
+
+export default Footer;
